9e46084f88a46651808a38c111c53c3c5ab6a47a
Kriptikz <cruzxkriptikz@gmail.com>
2021-09-29 09:13:56 +0200

    Create a path and file using the std::fs and std::path

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,8 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 }
 
 fn generate_report() {
+    let path = std::path::Path::new("./report.docx");
+    let file = std::fs::File::create(&path).unwrap();
+
     
 }
