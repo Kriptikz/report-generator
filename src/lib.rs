5a638e6e207cfd9d86798292e7e9d07158c179bc
Kriptikz <cruzxkriptikz@gmail.com>
2021-09-29 09:33:12 +0200

    Set other docx table parameters: set_grid, layout, and indent

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,19 @@ fn generate_report() {
     let path = std::path::Path::new("./report.docx");
     let file = std::fs::File::create(&path).unwrap();
 
-    Docx::new().add_table(
-        Table::new(vec![
-            TableRow::new(vec![
-                TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Simple"))),
-                TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Table")))
-            ]),
-            TableRow::new(vec![
-                TableCell::new().add_paragraph(Paragraph::new()),
-                TableCell::new().add_paragraph(Paragraph::new())
-            ]),
-        ]),
-        
-    );
+    Docx::new()
+        .add_table(
+            Table::new(vec![
+                TableRow::new(vec![
+                    TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Simple"))),
+                    TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Table")))
+                ]),
+                TableRow::new(vec![
+                    TableCell::new().add_paragraph(Paragraph::new()),
+                    TableCell::new().add_paragraph(Paragraph::new())
+                ]),
+            ]) 
+            .set_grid(vec![2000, 4000, 2000])
+            .layout(TableLayoutType::Fixed)
+            .indent(0));
 }
