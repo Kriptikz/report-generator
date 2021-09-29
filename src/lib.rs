4ec11a3fe7ea183905442ac1ffea1a4fb19ed4b0
Kriptikz <cruzxkriptikz@gmail.com>
2021-09-29 09:26:55 +0200

    Add a simple table to the document

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,17 @@ fn generate_report() {
     let path = std::path::Path::new("./report.docx");
     let file = std::fs::File::create(&path).unwrap();
 
-    Docx::new();
+    Docx::new().add_table(
+        Table::new(vec![
+            TableRow::new(vec![
+                TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Simple"))),
+                TableCell::new().add_paragraph(Paragraph::new().add_run(Run::new().add_text("Table")))
+            ]),
+            TableRow::new(vec![
+                TableCell::new().add_paragraph(Paragraph::new()),
+                TableCell::new().add_paragraph(Paragraph::new())
+            ]),
+        ]),
+        
+    );
 }
