e7a0758805186f8234735f2382fd04b6809d3283
Kriptikz <cruzxkriptikz@gmail.com>
2021-10-15 07:02:46 +0200

    Created basic sample output with hardcoded data

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,70 @@ fn generate_report() -> Result<(), DocxError> {
         .add_table(
             Table::new(vec![
                 TableRow::new(vec![
-                    make_cell("Simple"),
-                    make_cell("Table")
+                    make_cell("Verbal Comprehension"),
+                    make_cell("Standard Score"),
+                    make_cell("Perceptual Reasoning"),
+                    make_cell("Standard Score"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Similarities (SI)"),
+                    make_cell("16"),
+                    make_cell("Block Design (BD)"),
+                    make_cell("12"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Vocabulary (VC)"),
+                    make_cell("13"),
+                    make_cell("Matrix Reasoning (MR)"),
+                    make_cell("10"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Information (IN)"),
+                    make_cell("11"),
+                    make_cell("Visual Puzzles (VP)"),
+                    make_cell("10"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Comprehension (CO)"),
+                    make_cell("x"),
+                    make_cell("Figure Weights (FW)"),
+                    make_cell("x"),
                 ]),
                 TableRow::new(vec![
                     make_cell(""),
-                    make_cell("")
+                    make_cell(""),
+                    make_cell("Picture Completion (PC)"),
+                    make_cell("x"),
+                ]),
+                TableRow::new(vec![
+                    make_cell(""),
+                    make_cell(""),
+                    make_cell(""),
+                    make_cell(""),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Working Memory"),
+                    make_cell("Standard Score"),
+                    make_cell("Processing Speed"),
+                    make_cell("Standard Score"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Digit Span (DS)"),
+                    make_cell("10"),
+                    make_cell("Symbol Search (SS)"),
+                    make_cell("10"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Arithmetic (AR)"),
+                    make_cell("9"),
+                    make_cell("Coding (CD)"),
+                    make_cell("8"),
+                ]),
+                TableRow::new(vec![
+                    make_cell("Letter-Number Seq. (LN)"),
+                    make_cell("10"),
+                    make_cell("Cancellation (CA)"),
+                    make_cell("x"),
                 ]),
             ]) 
             .set_grid(vec![2000, 4000, 2000])
