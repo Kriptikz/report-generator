265c3fbba34e5df28e2474d79f1bee6b43514950
Kriptikz <cruzxkriptikz@gmail.com>
2021-10-21 23:23:30 +0200

    Implemented basic get_input()

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::io;
 
 mod report;
 
@@ -7,9 +8,11 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     loop {
         println!("\nPlease enter a command, (\"help\" for a list of commands):");
 
-        let input: &str = &get_input()[..];
+        let input = get_input();
 
 
+
+        let input = &input[..];
         match input {
             "exit" => break,
             _ => println!("Error: Not maching command: {}", input),
@@ -19,6 +22,11 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn get_input() -> String{
-    String::from("get_input() not implemented.")
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+
+    input.trim().to_string()
 }
