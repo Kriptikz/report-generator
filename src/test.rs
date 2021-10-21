3ef2bca49825ac9d8392e9fb37eb546c71099cc2
Kriptikz <cruzxkriptikz@gmail.com>
2021-10-22 00:38:00 +0200

    Added PartialEq to each struct so we can use assert in tests

--- src/test.rs
+++ src/test.rs
@@ -6,13 +6,13 @@ struct Range {
     max: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct Test {
     name: String,
     indexes: Vec<Index>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct Index {
     name: String,
     initials: String,
@@ -20,7 +20,7 @@ struct Index {
     subtests: Vec<Subtest>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct Subtest {
     name: String,
     initials: String,
@@ -29,10 +29,40 @@ struct Subtest {
     charts: Vec<Chart>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct Chart {
     age_range: Range,
     scaled_score_range: Range,
     raw_score_maxes: Vec<u32>,
     percentile_ranks: Vec<f32>,
+}
+
+impl Test {
+    pub fn new(new_name: String, new_indexes: Vec<Index>) -> Test {
+        Test { 
+            name: new_name,
+            indexes: new_indexes,
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_test_no_indexes() {
+        let name = "name";
+
+        let test1 = Test {
+            name: name.to_string(),
+            indexes: Vec::new(),
+        };
+
+        let test2 = Test::new(name.to_string());
+
+        assert_eq!(test1, test2);
+    }
 }
