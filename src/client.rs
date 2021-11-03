379d11002acb7fafcf2733a9aa082dec6f762d63
Kriptikz <cruzxkriptikz@gmail.com>
2021-11-03 23:34:54 +0100

    Implemented get_test_scores for Client

--- src/client.rs
+++ src/client.rs
@@ -27,6 +27,14 @@ impl Client {
     fn get_name(&self) -> &String {
         &self.name
     }
+
+    fn get_test_scores(&self) -> Option<&Vec<TestScore>> {
+        if self.test_scores.len() > 0 {
+            return Some(&self.test_scores)
+        }
+
+        None
+    }
 }
 
 #[cfg(test)]
@@ -63,6 +71,57 @@ mod testing
             let name2 = client.get_name();
     
             assert_eq!(name.to_string(), *name2);
+        }
+
+        #[test]
+        fn get_test_scores() {
+            let client_name = "client name";
+            let client_age = 25;
+
+            let test_name = "test name";
+            let index_name = "index name";
+            let subtest_name = "subtest name";
+            let score: u32 = 35;
+            let mut test_scores_error: Vec<TestScore> = Vec::new();
+
+            let mut test_scores: Vec<TestScore> = Vec::new();
+            let test_score1 = TestScore {
+                test_name: test_name.to_string(),
+                index_name: index_name.to_string(),
+                subtest_name: subtest_name.to_string(),
+                score: score,
+            };
+            test_scores.push(test_score1);
+
+            let client = Client::new(client_name.to_string(), client_age, test_scores);
+
+            let test_score_error = TestScore {
+                test_name: String::from("Error"),
+                index_name: String::from("Error"),
+                subtest_name: String::from("Error"),
+                score: 0,
+            };
+
+            test_scores_error.push(test_score_error);
+
+            let test_scores2: &Vec<TestScore>;
+
+            match client.get_test_scores() {
+                Some(test_scores) => test_scores2 = &test_scores,
+                None => test_scores2 = &test_scores_error,
+            }
+
+            let mut test_scores: Vec<TestScore> = Vec::new();
+            let test_score1 = TestScore {
+                test_name: test_name.to_string(),
+                index_name: index_name.to_string(),
+                subtest_name: subtest_name.to_string(),
+                score: score,
+            };
+            test_scores.push(test_score1);
+
+            assert_eq!(test_scores, *test_scores2);
+
         }
     
     }
